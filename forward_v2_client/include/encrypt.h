39653b744317a92c0ea9c7bae4ffd0373da7bdfc
zhenxiongchen <7755120@gmail.com>
2019-02-05 21:35:21 +0800

    1111

---
+++ forward_v2_client/include/encrypt.h
@@ -0,0 +1,11 @@
+//
+// Created by deepglint on 19-2-4.
+//
+
+#ifndef GFW_ENCRYPT_H
+#define GFW_ENCRYPT_H
+#include <string>
+#include <string.h>
+void do_encryption(std::string& file_to_be_encrypt, std::string& encrypted_file);
+void do_decryption(std::string& file_to_be_decrpt, std::string& decrpted_file);
+#endif //GFW_ENCRYPT_H
